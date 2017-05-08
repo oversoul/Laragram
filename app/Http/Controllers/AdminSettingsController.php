63d5bd5ae3953514a8f28b1a40db8e1cf9636f0d
Amine <kurosoul.ae@gmail.com>
2017-05-08 20:04:38 +0100

    fix validation error

--- app/Http/Controllers/AdminSettingsController.php
+++ app/Http/Controllers/AdminSettingsController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers;
 use Auth;
 use App\Instagram\Instagram;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class AdminSettingsController extends Controller
 {
-    
-	use ValidatesRequests;
 
 
     public function index()
